3006e6e88ec74e10b52b6ada0c53467e68c310d4
wmouser <theturtlemage@yahoo.com>
2018-06-17 14:25:46 -0500

    add base files and create file structure
    This commit adds the initial files and file structure for the website

---
+++ timestamp.js
@@ -0,0 +1,12 @@
+<!-- hide script from old browsers
+
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
+
+// end hiding script from old browsers -->
+
+
